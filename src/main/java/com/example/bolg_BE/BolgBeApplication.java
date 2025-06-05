0fa7f8bb53f7c8ee25fcc0d1e64649d1ebf2949f
Levanbinh123 <levanbin1712@gmail.com>
2025-06-05 11:26:54 +0700

    first commit

---
+++ src/main/java/com/example/bolg_BE/BolgBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.bolg_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolgBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BolgBeApplication.class, args);
+	}
+
+}
